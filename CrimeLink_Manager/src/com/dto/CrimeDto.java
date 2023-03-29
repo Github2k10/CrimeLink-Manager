835a9be45444d9d51d2acc07b30a6fbd22469a62
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-03-30 01:44:53 +0530

    day-3

--- CrimeLink_Manager/src/com/dto/CrimeDto.java
+++ CrimeLink_Manager/src/com/dto/CrimeDto.java
@@ -1,5 +1,29 @@
 package com.dto;
 
-public interface CrimeDto {
+import java.sql.Date;
 
+public interface CrimeDto {
+	public int getCrime_id();
+
+	public void setCrime_id(int crime_id);
+
+	public String getDesc();
+
+	public void setDesc(String desc);
+
+	public String getVictim_name();
+
+	public void setVictim_name(String victim_name);
+
+	public String getPs_area();
+
+	public void setPs_area(String ps_area);
+
+	public Date getDate();
+
+	public void setDate(Date date);
+
+	public String getType();
+
+	public void setType(String type);
 }
