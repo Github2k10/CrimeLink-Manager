76dd1457ebadbb84a3e4b527e9d3321d00babd2f
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-03-30 01:18:59 +0530

    update

--- CrimeLink_Manager/src/com/dao/ConnectToDatabase.java
+++ CrimeLink_Manager/src/com/dao/ConnectToDatabase.java
@@ -6,9 +6,25 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class ConnectToDatabase {
+	
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	static Connection makeConnection() throws SQLException {
 		ResourceBundle bundle = ResourceBundle.getBundle("dbdetails");
 		
 		return DriverManager.getConnection(bundle.getString("url"), bundle.getString("username"), bundle.getString("password"));
+	}
+	
+	static void closeConnection(Connection connection) throws SQLException {
+		if(connection != null) {
+			connection.close();
+		}
 	}
 }
