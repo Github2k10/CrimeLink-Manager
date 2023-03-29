1bbfb77722477497688c111c4264d39015c15c27
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-03-30 01:17:27 +0530

    update

--- CrimeLink_Manager/src/com/dao/ConnectToDatabase.java
+++ CrimeLink_Manager/src/com/dao/ConnectToDatabase.java
@@ -1,5 +1,14 @@
 package com.dao;
 
-public class ConnectToDatabase {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
+public class ConnectToDatabase {
+	static Connection makeConnection() throws SQLException {
+		ResourceBundle bundle = ResourceBundle.getBundle("dbdetails");
+		
+		return DriverManager.getConnection(bundle.getString("url"), bundle.getString("username"), bundle.getString("password"));
+	}
 }
