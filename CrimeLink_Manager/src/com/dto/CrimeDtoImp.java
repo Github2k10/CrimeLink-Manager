10956ab34887b11d024f786c485fb7ff5589b54e
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-03-30 01:45:54 +0530

    day-3

--- CrimeLink_Manager/src/com/dto/CrimeDtoImp.java
+++ CrimeLink_Manager/src/com/dto/CrimeDtoImp.java
@@ -21,50 +21,62 @@ public class CrimeDtoImp implements CrimeDto{
 		this.type = type;
 	}
 
+	@Override
 	public int getCrime_id() {
 		return crime_id;
 	}
 
+	@Override
 	public void setCrime_id(int crime_id) {
 		this.crime_id = crime_id;
 	}
 
+	@Override
 	public String getDesc() {
 		return desc;
 	}
 
+	@Override
 	public void setDesc(String desc) {
 		this.desc = desc;
 	}
 
+	@Override
 	public String getVictim_name() {
 		return victim_name;
 	}
 
+	@Override
 	public void setVictim_name(String victim_name) {
 		this.victim_name = victim_name;
 	}
 
+	@Override
 	public String getPs_area() {
 		return ps_area;
 	}
 
+	@Override
 	public void setPs_area(String ps_area) {
 		this.ps_area = ps_area;
 	}
 
+	@Override
 	public Date getDate() {
 		return date;
 	}
 
+	@Override
 	public void setDate(Date date) {
 		this.date = date;
 	}
 
+	@Override
 	public String getType() {
 		return type;
 	}
 
+	@Override
 	public void setType(String type) {
 		this.type = type;
 	}
