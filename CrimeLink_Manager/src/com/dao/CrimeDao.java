604e37676bfeb7a6a2a35a2e6f16dbed568c996b
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-03-30 16:13:16 +0530

    update

---
+++ CrimeLink_Manager/src/com/dao/CrimeDao.java
@@ -0,0 +1,11 @@
+package com.dao;
+
+import java.sql.Date;
+
+import com.dto.CrimeDto;
+
+public interface CrimeDao {
+	public boolean addCrime(CrimeDto crime);
+	public boolean updateCrime(int crime_id, String desc, String name, String area, Date date, String type);
+	public boolean deleteCrime(int crime_id);
+}
