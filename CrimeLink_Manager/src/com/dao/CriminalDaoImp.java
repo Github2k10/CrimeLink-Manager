5e14d692f4d9b230a3dce6b83ec2aa71cb581ee2
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-03-30 17:46:05 +0530

    update

--- CrimeLink_Manager/src/com/dao/CriminalDaoImp.java
+++ CrimeLink_Manager/src/com/dao/CriminalDaoImp.java
@@ -1,5 +1,53 @@
 package com.dao;
 
-public class CriminalDaoImp {
+import java.sql.Date;
+
+import com.dto.CriminalDto;
+
+public class CriminalDaoImp implements CriminalDao{
+	public CriminalDaoImp() {}
+
+	@Override
+	public boolean addCriminal(CriminalDto criminal) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateCriminal(int criminal_id, String name, Date date, String gender, String mark,
+			Date first_arrest, String arrestArea) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean assignCrime(int crime_id, int criminal_id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeCrime(int crime_d, int criminal_id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteCriminal(int criminal_id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public CriminalDto searchCriminalByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CriminalDto searchCriminalByDescription(String desc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
