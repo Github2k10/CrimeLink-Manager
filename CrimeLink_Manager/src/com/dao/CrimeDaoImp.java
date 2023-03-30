dfc1355c54bd33586c2c042a4f0a270e13d0f1b3
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-03-30 17:45:33 +0530

    update

--- CrimeLink_Manager/src/com/dao/CrimeDaoImp.java
+++ CrimeLink_Manager/src/com/dao/CrimeDaoImp.java
@@ -1,5 +1,41 @@
 package com.dao;
 
-public class CrimeDaoImp {
+import java.sql.Date;
+
+import com.dto.CrimeDto;
+
+public class CrimeDaoImp implements CrimeDao{
+	
+	public CrimeDaoImp() {}
+
+	@Override
+	public boolean addCrime(CrimeDto crime) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateCrime(int crime_id, String desc, String name, String area, Date date, String type) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteCrime(int crime_id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void showTotalCrimeForEachPS() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showTotalCrimeForDateRange(Date start, Date end) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
