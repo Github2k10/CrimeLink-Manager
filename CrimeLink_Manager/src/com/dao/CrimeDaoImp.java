e7e036970bd5b3ee84bd15696c1e42ef35a21173
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-03-30 18:43:25 +0530

    update

--- CrimeLink_Manager/src/com/dao/CrimeDaoImp.java
+++ CrimeLink_Manager/src/com/dao/CrimeDaoImp.java
@@ -1,6 +1,9 @@
 package com.dao;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import com.dto.CrimeDto;
 
@@ -10,7 +13,31 @@ public class CrimeDaoImp implements CrimeDao{
 
 	@Override
 	public boolean addCrime(CrimeDto crime) {
-		// TODO Auto-generated method stub
+		Connection connection = null;
+		
+		try {
+			connection = ConnectToDatabase.makeConnection();
+			String query = "inert into crime (description, victim_name, ps_area, c_date, type) values (?, ?, ?, ?, ?);";
+			
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, crime.getDesc());
+			statement.setString(2, crime.getVictim_name());
+			statement.setString(3, crime.getPs_area());
+			statement.setDate(4, crime.getDate());
+			statement.setString(5, crime.getType());
+			
+			statement.executeUpdate();
+			
+			return true;
+		} catch (SQLException e) {
+			
+		} finally {
+			try {
+				ConnectToDatabase.closeConnection(connection);
+			} catch (SQLException e) {
+				
+			}
+		}
 		return false;
 	}
 
