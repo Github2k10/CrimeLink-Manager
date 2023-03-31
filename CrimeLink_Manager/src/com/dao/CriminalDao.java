5fd112e23f9730dc443a83201343b3b90d0f07eb
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-04-01 00:05:56 +0530

    update

--- CrimeLink_Manager/src/com/dao/CriminalDao.java
+++ CrimeLink_Manager/src/com/dao/CriminalDao.java
@@ -12,7 +12,7 @@ public interface CriminalDao {
 	public boolean addCriminal(CriminalDto criminal) throws InvalidDataException, SomeThingWentWrongException;
 	public boolean updateCriminal(int criminal_id, String name, Date date, String gender, String mark, Date first_arrest, String arrestArea) throws CriminalNotFoundException, SomeThingWentWrongException;
 	public boolean assignCrime(int crime_id, int criminal_id) throws CriminalNotFoundException, SomeThingWentWrongException;
-	public boolean removeCrime(int crime_d, int criminal_id);
+	public boolean removeCrime(int crime_d, int criminal_id) throws CriminalNotFoundException, SomeThingWentWrongException;
 	public boolean deleteCriminal(int criminal_id);
 	public List<CriminalDto> searchCriminalByName(String name);
 }
