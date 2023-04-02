ad0bf60288d3fc44753dad8db6abee5c676127af
Ankit Kumar <65845325+Github2k10@users.noreply.github.com>
2023-04-02 15:16:41 +0530

    update

--- CrimeLink_Manager/src/com/ui/MainUI.java
+++ CrimeLink_Manager/src/com/ui/MainUI.java
@@ -1,29 +1,12 @@
 package com.ui;
 
-import java.sql.Date;
-import java.util.List;
 
-import com.dao.CrimeDao;
-import com.dao.CrimeDaoImp;
-import com.dao.CriminalDao;
-import com.dao.CriminalDaoImp;
-import com.dto.CrimeDto;
-import com.dto.CriminalDto;
-import com.exception.CrimeNotFoundException;
-import com.exception.CriminalNotFoundException;
-import com.exception.SomeThingWentWrongException;
 
 public class MainUI {
 
 	public static void main(String[] args) {
-		CriminalDao criminalDao = new CriminalDaoImp();
-		CrimeDao crimeDao = new CrimeDaoImp();
-		
-		try {
-			crimeDao.showTotalCrimeForEachPS("2011", "2014");
-		} catch (SomeThingWentWrongException | CrimeNotFoundException e) {
-			System.out.print(e.getMessage());
-		}
+		System.out.println("Here's some text");
+		System.out.println("\u001B[31m" + "and now the text is red");
 	}
 
 }
